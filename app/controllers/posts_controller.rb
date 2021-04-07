6cb283919db9ca0c38b365c0b7ba44c7913f3f8e
pi-koma <ysk.home0114@gmail.com>
2021-04-07 18:22:43 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,15 @@
+class PostsController < ApplicationController
+
+  def index  # indexアクションを定義した
+    @posts = Post.all #"これはコントローラーで定義したインスタンス変数を確認するための文字列です"
+ end
+
+ def new
+ end
+
+ def create
+  Post.create(content: params[:content])
+
+ end
+
+end
